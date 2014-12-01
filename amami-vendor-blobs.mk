f0fe579033d73de2ee26c1a9353153a72a2216fa
martincz <martincz@mokeedev.com>
2014-12-02 01:47:06 +0800

    update vendor files
    Change-Id: I1f3b92868ad46275b28916f558f10e90090d249b

--- amami-vendor-blobs.mk
+++ amami-vendor-blobs.mk
@@ -16,7 +16,6 @@
 
 PRODUCT_COPY_FILES += \
     vendor/sony/amami/proprietary/bin/chargemon:system/bin/chargemon \
-    vendor/sony/amami/proprietary/bin/illumination_service:system/bin/illumination_service \
     vendor/sony/amami/proprietary/bin/sensors.qcom:system/bin/sensors.qcom \
     vendor/sony/amami/proprietary/bin/updatemiscta:system/bin/updatemiscta \
     vendor/sony/amami/proprietary/etc/acdbdata/Liquid/Liquid_Handset_cal.acdb:system/etc/acdbdata/Liquid/Liquid_Handset_cal.acdb \
@@ -33,10 +32,8 @@ PRODUCT_COPY_FILES += \
     vendor/sony/amami/proprietary/etc/flashled_calc_parameters.cfg:system/etc/flashled_calc_parameters.cfg \
     vendor/sony/amami/proprietary/etc/sensor_def_qcomdev.conf:system/etc/sensor_def_qcomdev.conf \
     vendor/sony/amami/proprietary/etc/sensors_calib.conf:system/etc/sensors_calib.conf \
-    vendor/sony/amami/proprietary/etc/sysmon.cfg:system/etc/sysmon.cfg \
     vendor/sony/amami/proprietary/lib/libcald_hal.so:system/lib/libcald_hal.so \
     vendor/sony/amami/proprietary/lib/libcameralight.so:system/lib/libcameralight.so \
-    vendor/sony/amami/proprietary/lib/libsysmon_jni.so:system/lib/libsysmon_jni.so \
     vendor/sony/amami/proprietary/usr/idc/clearpad.idc:system/usr/idc/clearpad.idc \
     vendor/sony/amami/proprietary/vendor/camera/LGI02BN1.dat:system/vendor/camera/LGI02BN1.dat \
     vendor/sony/amami/proprietary/vendor/camera/LGI02BN1_CACAO.dat:system/vendor/camera/LGI02BN1_CACAO.dat \
